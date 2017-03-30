292c936c5293808f3c2b600f64199f284c10a1ed
Ryan Anguiano <ryan.anguiano@gmail.com>
2017-03-30 19:09:16 -0400

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='redis_timeseries',
-    version='0.1.0',
+    version='0.1.1',
     description="Timeseries API built on top of Redis",
     long_description=readme + '\n\n' + history,
     author="Ryan Anguiano",
